b174d69b6bab2de65350de5d43734e7440909bca
Velo_student <NA>
2015-11-09 17:14:28 +0000

    changes made to reorder.cpp/.h and Makefile

--- Toby_GWT_Algorithm/reorder.cpp
+++ Toby_GWT_Algorithm/reorder.cpp
@@ -10,35 +10,12 @@ frame str_to_frame(string signal)
   return _frame;
 }
 
-// spp frame_to_spp(frame frame_in, int spp_no)
-// {
-//   spp temp;
 
-//   for (int i(0); i < 30; i++)
-//     temp[i] = frame_in[30*spp_no + i];
-    
-//   return temp;
-// }
 
-// frame attach_spp_to_frame(frame frame_in, spp attachment, int spp_no)
-// {
-//   frame frame_out;
-
-//   frame_out = frame_in;
-//   // for (int i(0); i < 8; i++)
-//   //frame_out[120+i] = frame_in[120+i];
-  
-
-//   for (int i(0); i < 30; i++)
-//     frame_out[30*spp_no+i] = attachment[i];
-
-//   return frame_out;
-}
-
-frame Reorder(frane TX_DATA_IN)
+frame Reorder(frame TX_DATA_IN)
 {
- 
-TX_DATA_OUT = TX_DATA_IN
+  frame TX_DATA_OUT; 
+  TX_DATA_OUT = TX_DATA_IN;
 
   return TX_DATA_OUT; //COMMON_FRAME
 }
