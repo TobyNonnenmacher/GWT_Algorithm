fd50dfaecb502310f6e366cf1180e319b2657295
Velo_student <NA>
2015-11-13 14:17:09 +0000

    more on python file

--- Toby_GWT_Algorithm/TwosComplement.py
+++ Toby_GWT_Algorithm/TwosComplement.py
@@ -1,11 +1,18 @@
 #!/usr/bin/python
 
-def to_twoscomplement(bits, value):
+def to_twoscomplement(value):
     if value < 0:
-        value = ( 1<<bits ) + value
-    formatstring = '{:0%ib}' % bits
+        value = ( 1<<16 ) + value
+    formatstring = '{:0%ib}' % 16
     return formatstring.format(value)
 
 
-a = to_twoscomplement(16, 77)
-print a
+
+#with open("gwt_pll_1000frames.txt") as f:
+ #   for line in f:
+  #      <to_twoscomplement with line>
+
+
+#a = to_twoscomplement(-77)
+#print(a)
+
